e79656f94f8dade42d117782bad4eb8e335f1dc7
matteokfg <67129306+matteokfg@users.noreply.github.com>
2025-03-25 23:24:21 -0300

    Update test_projeto.py
    feat: creation of tests

--- test_projeto.py
+++ test_projeto.py
@@ -1,10 +1,22 @@
 import pytest
+from projeto import recebe_prompt, conecta_googlegenai, retorna_resposta
 
-def test_retorna_resposta():
-    None
-
-def test_conecta_googlegenai():
-    None
+#preencher "" com prompt de exemplo/teste
 
 def test_recebe_prompt():
-    None
+    assert recebe_prompt("Sou um prompt") == (True, "Sou um prompt")
+    assert recebe_prompt("") == (False, "Prompt com erro")
+    assert recebe_prompt(7) == (False, "Prompt com erro")
+    assert recebe_prompt(True) == (False, "Prompt com erro")
+    assert recebe_prompt([]) == (False, "Prompt com erro")
+    assert recebe_prompt({}) == (False, "Prompt com erro") 
+
+def test_conecta_googlegenai():
+    assert conecta_googlegenai("") == {'returnCode': 200, 'returnData': ""}
+    assert conecta_googlegenai("") == {'returnCode': 400}
+
+def test_retorna_resposta():
+    assert retorna_resposta(200, "") == ""
+    assert retorna_resposta(200, "") == "Erro na resposta" #resposta não contem palavra chave
+    assert retorna_resposta(400, "") == "Erro na resposta"
+    assert retorna_resposta(False, "Prompt com erro") == "Prompt com erro"
