387dea919628bee3db4539e450135f03c6afa3b4
matteokfg <67129306+matteokfg@users.noreply.github.com>
2025-03-25 22:52:49 -0300

    Arrumar diretorio e imports
    bug: organize the mkdir and created imports

---
+++ projeto.py
@@ -0,0 +1,12 @@
+import google.generativeai as genai
+from token import ClientGenAITokenClass as Token
+
+
+def recebe_prompt():
+    None
+
+def conecta_googlegenai():
+    None
+
+def retorna_resposta():
+    None
