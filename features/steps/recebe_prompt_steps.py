82a98a2dd57cdca74e3270427bb19bd7490a0df8
matteokfg <67129306+matteokfg@users.noreply.github.com>
2025-04-20 20:21:48 -0300

    fix: behave @given

--- features/steps/recebe_prompt_steps.py
+++ features/steps/recebe_prompt_steps.py
@@ -1,13 +1,13 @@
 from behave import given, when, then
 from projeto import recebe_prompt
 
-@given("o texto: {texto}")
-def definir_texto(context, texto):
-    context.texto = texto
+@given("o texto: {prompt}")
+def definir_texto(context, prompt):
+    context.prompt = prompt
 
 @when('eu valido')
 def validar_prompt(context):
-    context.resultado_booleano, context.resultado_texto = recebe_prompt(context.texto)
+    context.resultado_booleano, context.resultado_texto = recebe_prompt(context.prompt)
 
 @then("ele retorna {booleano} e {texto_resultado}")
 def verificar_resultado(context, booleano, texto_resultado):
